c17a7defdb0a95a13b327d710f96e1e4fa5e3377
tknr <geishagirlskoume@gmail.com>
2015-04-02 17:33:40 +0900

    init

--- make.sh
+++ make.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 DIR=$(cd $(dirname $0); pwd)
 cd $DIR
-DATE=`date +%Y_%m_%d` 
+DATE=`date +%Y_%m_%d`
+
+rm *.xpi
+
 cd Echofon/
 zip -r -9 Echofon.jar *
 cd ../
@@ -9,4 +12,4 @@ cp Echofon.jar src/chrome/
 cd src/
 zip -r -9 mod_echofon_${DATE}.xpi *
 cd ../
-mv *.xpi .
+mv -v *.xpi .
